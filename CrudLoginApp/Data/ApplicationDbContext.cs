153d2bd2d98c6cc969b2d1cd1967816dd33a552f
Esteban1211 <spellman3@live.com>
2026-04-09 23:32:39 -0400

    Add project files.

---
+++ CrudLoginApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CrudLoginApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudLoginApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ CrudLoginApp/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudLoginApp.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        public double Precio { get; set; }
+    }
+
+}
